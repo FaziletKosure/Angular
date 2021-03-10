01400aaf2f113a273df1fa632fe2465b13c24f09
FaziletKosure <faziletkosure1@gmail.com>
2021-03-10 19:59:43 +0100

    created models folder

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,3 +1,5 @@
+import { Todo } from './../../models/todo';
+import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  product1= {productId:1,productName:"Bardak",categoryId:1,unitPrice:5}
-  product2= {productId:2,productName:"Kalem",categoryId:1,unitPrice:5}
-  product3= {productId:3,productName:"Monitor",categoryId:1,unitPrice:5}
-  product4= {productId:4,productName:"Kitap",categoryId:1,unitPrice:5}
-  product5= {productId:5,productName:"Sandalye",categoryId:1,unitPrice:5}
+  // product1= {productId:1,productName:"Bardak",categoryId:1,unitPrice:5,unitsInStock}
+  // product2= {productId:2,productName:"Kalem",categoryId:1,unitPrice:5}
+  // product3= {productId:3,productName:"Monitor",categoryId:1,unitPrice:5}
+  // product4= {productId:4,productName:"Kitap",categoryId:1,unitPrice:5}
+  // product5= {productId:5,productName:"Sandalye",categoryId:1,unitPrice:5}
 
-  products = [this.product1,this.product2,this.product3,this.product4,this.product5];
+  products:Product[] = [];
+  todos:Todo[]=[];
   
   constructor() { }
 
